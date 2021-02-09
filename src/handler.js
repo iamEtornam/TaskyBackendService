6692a8e0e8b8b84c0259aba1e12f7f51353aa752
Etornam <sunumacbright@gmail.com>
2021-02-09 16:43:38 +0000

    remove default hello code

--- src/handler.js
+++ src/handler.js
@@ -6,32 +6,32 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
 
-module.exports.hello = async event => {
-  return {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, OPTIONS',
-      },
-      body: JSON.stringify({
-        status: true,
-        message: 'Data here'
-      }),
-    };
+function verifyToken(token) {
+    try {
+        const decodedToken = await admin.auth().verifyIdToken(token)
+        if(decodedToken){
+            return true;
+        }else{
+            return false;
+        }
+    } catch (error) {
+        return false;
+    }
+}
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
-
-
+/// verify token code from client and return user data
 module.exports.login = async event => {
     const requestBody = JSON.parse(event.body);
     const token = requestBody.token;
 
 try{
-    const decodedToken = await admin.auth().verifyIdToken(token)
+    const decodedToken = verifyToken(token)
     if(decodedToken){
         const uid = decodedToken.uid;
+//get user data from database
+
+
+
         return {
             statusCode: 200,
             headers: {
