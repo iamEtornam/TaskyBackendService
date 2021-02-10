6612fb357f73924c63086f876b9b60a47cfe690c
Etornam <sunumacbright@gmail.com>
2021-02-10 00:04:01 +0000

    Update handler.js

--- src/handler.js
+++ src/handler.js
@@ -1,17 +1,24 @@
 'use strict';
 const admin = require("firebase-admin");
 const serviceAccount = require("../serviceAccountKey.json");
+const db = require('./models')
+const {
+    Op
+} = require("sequelize");
+const Organizations = db.organization;
+const Users = db.user;
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
 
-function verifyToken(token) {
+async function verifyToken(token) {
+    console.log(token);
     try {
         const decodedToken = await admin.auth().verifyIdToken(token)
-        if(decodedToken){
+        if (decodedToken) {
             return true;
-        }else{
+        } else {
             return false;
         }
     } catch (error) {
@@ -19,37 +26,83 @@ function verifyToken(token) {
     }
 }
 
+function updateOrCreate(model, values, condition) {
+    return model
+        .findOne({
+            where: condition
+        })
+        .then(function (obj) {
+            // update
+            if (obj)
+                return obj.update(values);
+            // insert
+            return model.create(values);
+        })
+}
+
 /// verify token code from client and return user data
 module.exports.login = async event => {
     const requestBody = JSON.parse(event.body);
     const token = requestBody.token;
 
-try{
-    const decodedToken = verifyToken(token)
-    if(decodedToken){
-        const uid = decodedToken.uid;
-//get user data from database
+    try {
+        const decodedToken = await admin.auth().verifyIdToken(token)
+        if (decodedToken) {
+            const uid = decodedToken.uid;
 
+            //get user data from database
+            const user = await updateOrCreate(Users, {
+                name: decodedToken.name,
+                picture: decodedToken.picture,
+                user_id: uid,
+                email: decodedToken.email,
+                sign_in_provider: decodedToken.firebase.sign_in_provider,
+                auth_token: token
+            }, {
+                email: decodedToken.email
+            })
 
-
-        return {
-            statusCode: 200,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'GET, OPTIONS',
-            },
-            body: JSON.stringify({
-                status: true,
-                message: 'Authentication successful',
-                data: {
-                    name: decodedToken.name,
-                    picture:decodedToken.picture,
-                    user_id:decodedToken.user_id,
-                    email:decodedToken.email
-                }
-            }),
-        };
-    }else {
+            if (user) {
+                return {
+                    statusCode: 200,
+                    headers: {
+                        'Access-Control-Allow-Origin': '*',
+                        'Access-Control-Allow-Methods': 'GET, OPTIONS',
+                    },
+                    body: JSON.stringify({
+                        status: true,
+                        message: 'Authentication successful',
+                        data: user
+                    }),
+                };
+            } else {
+                return {
+                    statusCode: 400,
+                    headers: {
+                        'Access-Control-Allow-Origin': '*',
+                        'Access-Control-Allow-Methods': 'GET, OPTIONS',
+                    },
+                    body: JSON.stringify({
+                        status: false,
+                        message: 'Could not create user',
+                        data: decodedToken
+                    }),
+                };
+            }
+        } else {
+            return {
+                statusCode: 400,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'GET, OPTIONS',
+                },
+                body: JSON.stringify({
+                    status: false,
+                    message: 'Authentication failed.'
+                }),
+            };
+        }
+    } catch (e) {
         return {
             statusCode: 400,
             headers: {
@@ -58,21 +111,108 @@ try{
             },
             body: JSON.stringify({
                 status: false,
-                message: 'Authentication failed.'
+                message: e.message
             }),
         };
     }
-}catch (e) {
-    return {
-        statusCode: 400,
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET, OPTIONS',
-        },
-        body: JSON.stringify({
-            status: false,
-            message: e.message
-        }),
-    };
-}
 };
+
+
+/// get all organizations
+module.exports.getOrganizations = async event => {
+    try {
+        const organizations = await Organizations.findAll({
+            order: [
+                ['id', 'DESC'],
+            ]
+        })
+        if (organizations) {
+            return {
+                statusCode: 200,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'GET, OPTIONS',
+                },
+                body: JSON.stringify({
+                    status: true,
+                    message: 'List of all organizations',
+                    data: organizations
+                }),
+            }
+        } else {
+            return {
+                statusCode: 404,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'GET, OPTIONS',
+                },
+                body: JSON.stringify({
+                    status: false,
+                    message: 'organizations not found'
+                }),
+            }
+        }
+    } catch (error) {
+        return {
+            statusCode: 400,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, OPTIONS',
+            },
+            body: JSON.stringify({
+                status: false,
+                message: error.message
+            }),
+        };
+    }
+}
+
+/// get an organization by id
+module.exports.getOrganizationById = async event => {
+    try {
+        const organizations = await Organizations.findOne({
+            where: {
+                id: event.pathParameters.id
+            },
+            include: ["members"],
+        })
+        if (organizations) {
+            return {
+                statusCode: 200,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'GET, OPTIONS',
+                },
+                body: JSON.stringify({
+                    status: true,
+                    message: 'List of all organizations',
+                    data: organizations
+                }),
+            }
+        } else {
+            return {
+                statusCode: 404,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'GET, OPTIONS',
+                },
+                body: JSON.stringify({
+                    status: false,
+                    message: 'organizations not found'
+                }),
+            }
+        }
+    } catch (error) {
+        return {
+            statusCode: 400,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, OPTIONS',
+            },
+            body: JSON.stringify({
+                status: false,
+                message: error.message
+            }),
+        };
+    }
+}
