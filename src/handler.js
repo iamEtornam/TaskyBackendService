14509fb537c79387fd9c58b04173370d95c08a01
Etornam <sunumacbright@gmail.com>
2021-02-10 00:51:30 +0000

    add authorization to endpoints and add user department update

--- src/handler.js
+++ src/handler.js
@@ -12,17 +12,13 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
 
-async function verifyToken(token) {
-    console.log(token);
+async function verifyToken(authorization) {
+   
     try {
-        const decodedToken = await admin.auth().verifyIdToken(token)
-        if (decodedToken) {
-            return true;
-        } else {
-            return false;
-        }
+        const result = authorization.split(' ')
+        return await admin.auth().verifyIdToken(result[1])
     } catch (error) {
-        return false;
+        return;
     }
 }
 
@@ -64,7 +60,7 @@ module.exports.login = async event => {
 
             if (user) {
                 return {
-                    statusCode: 200,
+                    statusCode: 201,
                     headers: {
                         'Access-Control-Allow-Origin': '*',
                         'Access-Control-Allow-Methods': 'GET, OPTIONS',
@@ -121,6 +117,20 @@ module.exports.login = async event => {
 /// get all organizations
 module.exports.getOrganizations = async event => {
     try {
+        const token = await verifyToken(event.headers.Authorization)
+        if(token == null){
+            return {
+                statusCode: 401,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'GET, OPTIONS',
+                },
+                body: JSON.stringify({
+                    status: false,
+                    message: 'Unauthorized'
+                }),
+            }
+        }
         const organizations = await Organizations.findAll({
             order: [
                 ['id', 'DESC'],
@@ -170,6 +180,20 @@ module.exports.getOrganizations = async event => {
 /// get an organization by id
 module.exports.getOrganizationById = async event => {
     try {
+        const token = await verifyToken(event.headers.Authorization)
+        if(token == null){
+            return {
+                statusCode: 401,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'GET, OPTIONS',
+                },
+                body: JSON.stringify({
+                    status: false,
+                    message: 'Unauthorized'
+                }),
+            }
+        }
         const organizations = await Organizations.findOne({
             where: {
                 id: event.pathParameters.id
@@ -208,6 +232,79 @@ module.exports.getOrganizationById = async event => {
             headers: {
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Methods': 'GET, OPTIONS',
+            },
+            body: JSON.stringify({
+                status: false,
+                message: error.message
+            }),
+        };
+    }
+}
+
+/// update user department
+module.exports.updateUserDepartment = async event => {
+    const requestBody = JSON.parse(event.body);
+    const department = requestBody.department;
+
+    try {
+        const token = await verifyToken(event.headers.Authorization)
+        if(token == null){
+            return {
+                statusCode: 401,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'PUT, OPTIONS',
+                },
+                body: JSON.stringify({
+                    status: false,
+                    message: 'Unauthorized'
+                }),
+            }
+        }
+        const userId = await Users.update({department: department},{
+            where: {
+                user_id: token.uid
+            }
+        })
+        console.log(userId,'userId');
+        if (userId) {
+            const updatedUser = await Users.findOne({
+                where: {
+                    id: userId[0]
+                },
+                include: ["organization"],
+            })
+            return {
+                statusCode: 200,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'PUT, OPTIONS',
+                },
+                body: JSON.stringify({
+                    status: true,
+                    message: 'User information updated',
+                    data: updatedUser
+                }),
+            }
+        } else {
+            return {
+                statusCode: 404,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'PUT, OPTIONS',
+                },
+                body: JSON.stringify({
+                    status: false,
+                    message: 'user not found'
+                }),
+            }
+        }
+    } catch (error) {
+        return {
+            statusCode: 400,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'PUT, OPTIONS',
             },
             body: JSON.stringify({
                 status: false,
