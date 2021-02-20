beb13b2a4d3c2fab7f1a7e36d54e469c431a7262
Etornam <sunumacbright@gmail.com>
2021-02-20 12:22:17 +0000

    Create 20210219183452-create-task.js

---
+++ src/migrations/20210219183452-create-task.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('tasks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      due_date: {
+        type: Sequelize.STRING
+      },
+      is_reminder: {
+        type: Sequelize.BOOLEAN
+      },
+      assignees: {
+        type: Sequelize.ARRAY(Sequelize.JSONB)
+      },
+      organization_id: {
+        type: Sequelize.INTEGER
+      },
+      created_by: {
+        type: Sequelize.INTEGER
+      },
+      department: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('tasks');
+  }
+};
