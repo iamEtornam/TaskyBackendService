0f0fda6cdee962b3cc93132fbd8ed44c780daced
Etornam <sunumacbright@gmail.com>
2022-10-21 12:23:04 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,32 @@
 const express = require('express');
 const logger = require('morgan');
-const mailgun = require("mailgun-js");
-const env = process.env.NODE_ENV || "mailserver";
-const mailConfig = require(__dirname + "/config/config.json")[env];
+const Sentry = require("@sentry/node");
 const config = require(__dirname + "/config/config.json")["sentry"];
 const admin = require("firebase-admin");
 const serviceAccount = require("../serviceAccountKey.json");
 
 const indexRouter = require('./routes/index');
 
+Sentry.init({ dsn: config.apiKey });
+
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-Sentry.AWSLambda.init({
-  dsn: config.apiKey,
-
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
-  tracesSampleRate: 1.0,
-});
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.errorHandler());
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-const mg = mailgun({
-  apiKey: mailConfig.apiKey,
-  domain: mailConfig.domain,
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.statusCode = 500;
+  res.end(res.sentry + "\n");
 });
 
 
